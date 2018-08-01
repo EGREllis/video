78f5bc891a77f48dd7a86db613332c012529989a
egrellis <edwardellis.egre@gmail.com>
2018-08-01 13:06:54 +0100

    Simple maven archetype

---
+++ src/main/java/net/ellise/App.java
@@ -0,0 +1,13 @@
+package net.ellise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
